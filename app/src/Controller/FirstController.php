a5f7b6915ddf71d6c02a5dc1355580bbcc83516b
Zuzanna <zuzanna.jacewicz@gmail.com>
2019-06-03 22:20:56 +0200

    commit!

---
+++ app/src/Controller/FirstController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class FirstClass
+ */
+
+class FirstController extends AbstractController
+{
+    /**
+     * @param $name
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/{name}")
+     */
+
+    public function firstFunction($name): Response
+    {
+        return $this->render(
+            'first/index.html.twig',
+            ['name' => $name]
+        );
+    }
+
+}
